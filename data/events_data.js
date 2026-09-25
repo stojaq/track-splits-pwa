c8b3fe74d61c1b121f5d83d521f0934c66750a2d
yosoycriss <cristian.qorri@studio.unibo.it>
2026-09-25 16:53:25 +0200

    aggiunti eventi

--- data/events_data.js
+++ data/events_data.js
@@ -8,7 +8,7 @@ const upcomingEvents = [
     link: "https://www.fidal.it/calendario/53a-MARCIALONGA-SUL-RUBICONE-(4-9-Km-e-14-Km)/REG44726"
   },
   {
-    id: 3,
+    id: 2,
     name: "52ª Cheursa di Bech",
     date: "2026-11-15T09:00:00",
     location: "Santarcangelo di Romagna (RN)",
@@ -16,11 +16,83 @@ const upcomingEvents = [
     link: "https://www.endu.net/it/events/cheursa-di-bech"
   },
   {
-    id: 2,
+    id: 3,
     name: "49° Giro dei Gessi",
     date: "2026-11-01T09:30:00",
     location: "Cesena (FC)",
     type: "Corsa Podistica",
     link: "https://podistiche.it/gare/49-giro-dei-gessi-2026"
+  },
+  {
+    id: 4,
+    name: "57ª Giro dei Tre Monti",
+    date: "2026-10-04T09:00:00",
+    location: "Imola (BO)",
+    type: "Corsa Regionale su strada",
+    link: "https://www.fidal.it/calendario/57%5E-GIRO-DEI-TRE-MONTI-(15-3-Km)/REG42707"
+  },
+  {
+    id: 5,
+    name: "Maratona di Berlino",
+    date: "2026-09-27T09:15:00",
+    location: "Berlino (GER)",
+    type: "Maratona (World Major - 42,195 km)",
+    link: ""
+  },
+  {
+    id: 6,
+    name: "Maratona di Chicago",
+    date: "2026-10-11T07:30:00",
+    location: "Chicago (USA)",
+    type: "Maratona (World Major - 42,195 km)",
+    link: ""
+  },
+  {
+    id: 7,
+    name: "Rome Half Marathon",
+    date: "2026-10-18T08:30:00",
+    location: "Roma",
+    type: "Mezza Maratona (21,097 km)",
+    link: ""
+  },
+  {
+    id: 8,
+    name: "Valencia Half Marathon",
+    date: "2026-10-25T08:30:00",
+    location: "Valencia (ESP)",
+    type: "Mezza Maratona (21,097 km)",
+    link: ""
+  },
+  {
+    id: 9,
+    name: "New York City Marathon",
+    date: "2026-11-01T08:00:00",
+    location: "New York (USA)",
+    type: "Maratona (World Major - 42,195 km)",
+    link: ""
+  },
+  {
+    id: 10,
+    name: "Maratona di Valencia",
+    date: "2026-12-06T08:15:00",
+    location: "Valencia (ESP)",
+    type: "Maratona (42,195 km)",
+    link: ""
+  },
+  {
+    id: 11,
+    name: "10K Valencia Ibercaja by Kiprun",
+    date: "2027-01-10T09:30:00",
+    location: "Valencia (ESP)",
+    type: "10K su strada",
+    link: ""
+  },
+  {
+    id: 12,
+    name: "Golden Gala Pietro Mennea",
+    date: "2027-05-28T20:00:00",
+    location: "Roma, Stadio Olimpico",
+    type: "Diamond League",
+    link: "https://rome.diamondleague.com/"
   }
 ];
